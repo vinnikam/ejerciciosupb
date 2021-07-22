0ed244f04eb52ee6ed9b9738b091e31163bf480f
vinnik4m <g.briceno@uniandes.edu.co>
2021-07-22 10:09:02 -0500

    Vinni- Adicione funciones y uso

--- src/Vacia.java
+++ src/Vacia.java
@@ -1,4 +1,7 @@
 
+import java.util.List;
+
+
 /**
  *
  * @author Vinni
@@ -8,5 +11,27 @@ public class Vacia {
     int a;
     int b;
 
+    // numero de caracteres de una cadena.
+    public int longitudClaseDeHoy(String palabra) {
+        System.out.println(palabra);
+        return 9;
+    }
+
+    // imprimir los 10 primeros primos a partir de 1
+    public void primeros10Primos() {
+        System.out.println("primeros10Primos");
+    }
+
+    //imprimir numero pares entre dos numeros
+    public void pares(int num1, int num2){
+        System.out.println("pares");
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+    public List pares1(int num1, int num2){
+        System.out.println("pares1");
+         return null;
+    }
+    
     
 }
