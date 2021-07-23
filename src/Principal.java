aff41db89f52d695b47344d48c8ec8cc3453b270
vinnik4m <g.briceno@uniandes.edu.co>
2021-07-23 09:44:29 -0500

    Vinni- Finalizacion app suma

--- src/Principal.java
+++ src/Principal.java
@@ -6,8 +6,50 @@ import java.util.Scanner;
  * @author Vinni
  */
 public class Principal {
-
     public static void main(String[] arg) {
+        System.out.println("Iniciando ..");
+        Operaciones oper = new Operaciones();
+            
+        int num1 = oper.leerNumeroEntero("Digite un número (1) mayor a 0, si lo digito mal repita : ");
+        int num2 = oper.leerNumeroEntero("Digite un número (2) mayor a 0, si lo digito mal repita : ");
+
+        int resultado = oper.sumar(num1, num2);
+        System.out.println(" La suma entre " + num1 + " y  " + num2 + " = " + resultado);
+        
+        //System.out.println("lei "+num1);
+        System.out.println("Finalizando ..");
+
+    }
+    public static void main2(String[] arg) {
+        System.out.println("Iniciando ..");
+        boolean nu1ok = true;
+        boolean nu2ok = true;
+
+        Scanner lector = new Scanner(System.in);
+        int num1 =0;
+        do{
+            System.out.print(" Digite un número (1) mayor a 0, si lo digito mal repita : ");
+            num1 = lector.nextInt();
+        }
+        while (num1 <0);
+        
+        int num2 =0;
+        do{
+            System.out.print(" Digite un número (2) mayor a 0, si lo digito mal repita : ");
+            num2 = lector.nextInt();
+        }while (num2 <0);
+        
+        
+        Operaciones oper = new Operaciones();
+        int resultado = oper.sumar(num1, num2);
+        System.out.println(" La suma entre " + num1 + " y  " + num2 + " = " + resultado);
+
+        
+        //System.out.println("lei "+num1);
+        System.out.println("Finalizando ..");
+
+    }
+    public static void main1(String[] arg) {
         System.out.println("Iniciando ..");
         //1 pedir 2 datos numericos
         //2 validar que los numeros sean positivos        
