7bf6759413b77ab82608ac0f88f933ca3f85a78f
Vinni <vinni9@gmail.com>
2021-08-06 09:07:54 -0500

    Vinni- Fin Figuras

--- src/figuras/Cuadrado.java
+++ src/figuras/Cuadrado.java
@@ -12,13 +12,24 @@ package figuras;
 public class Cuadrado extends Figura{
     private double lado;
     
+    public Cuadrado(double lado, String nombre){
+        super(nombre);  // llamado al constructor papa public Figura(String nombre){
+        this.lado = lado;
+        
+    }
+    public Cuadrado(double lado){
+        super("Cuadrado de "+lado+" de lado");  // llamado al constructor papa public Figura(String nombre){
+        this.lado = lado;
+        
+    }
+    @Override
     public double calcularArea(){
         return lado * lado;
     }
     
-    public void asignarLado(double lado){
-        this.lado = lado;
-    }
+//    public void asignarLado(double lado){
+//        this.lado = lado;
+//    }
     public double obtenerLado(){
         return this.lado;
     }
